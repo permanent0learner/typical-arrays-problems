b0f6e62401fb722eda6f8d1361095d0c8d2a8d36
permanent0learner <iba0@ya.ru>
2021-03-16 00:13:16 +0300

    change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length)
+  {
+    return 0;
+  }
+  else {
+  let res = array[0];
+  for(let i= 0; i < array.length; i++)
+  {
+    if ((array[i]) < res)
+    {
+      res = array[i];
+    }
+
+  }
+  return res;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length)
+  {
+    return 0;
+  }
+  else {
+  let res = array[0];
+  for (let i= 0; i < array.length; i++)
+  {
+    if ((array[i]) > res)
+    {
+      res = array[i];
+    }
+
+  }
+  return res;
+  }
+     
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array) || !array.length)
+  {
+    return 0;
+  }
+  else{
+  let res = 0;
+  for(let i= 0; i < array.length; i++)
+  {
+    res += array[i]; 
+
+  }
+  return res/array.length;
+  }
+  
 }
